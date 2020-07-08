113e85af43c927973af36b053ab8cd134580f8aa
Владимир Рысев <vladimir.risew@yandex.ru>
2020-07-09 02:06:48 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Test.ViewModels.Home;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int rowCount = 5)
+        {
+            var model = new IndexViewModel();
+            model.Init(rowCount);
+            return View(model);
+        }
+    }
+}
---
+++ ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.ViewModels.Home
+{
+    public class IndexViewModel
+    {
+        [Display(Name ="Выбрать все")]
+        public bool SelectAll { get; set; }
+        public List<RowDto> Rows { get; set; }
+        public void Init(int rowCount)
+        {
+            Rows = new List<RowDto>();
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                Rows.Add(new RowDto());
+            }
+        }
+    }
+
+    public class RowDto
+    {
+        public bool Douwnload { get; set; }
+        public bool Delete { get; set; }
+        public bool Change { get; set; }
+        public bool Create { get; set; }
+        public bool Read { get; set; }
+    }
+}
